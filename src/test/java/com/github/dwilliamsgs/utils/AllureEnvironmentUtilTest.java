78adb43ff5bb8ce3c9c99e416d30a10998778e45
danielwilliams <daniel.williams@gymshark.com>
2022-03-03 09:20:54 +0000

    Use static import for directories

--- src/test/java/com/github/dwilliamsgs/utils/AllureEnvironmentUtilTest.java
+++ src/test/java/com/github/dwilliamsgs/utils/AllureEnvironmentUtilTest.java
@@ -6,7 +6,7 @@ import org.testng.annotations.Test;
 import java.io.File;
 import java.util.HashMap;
 
-import static com.github.dwilliamsgs.utils.AllureEnvironmentUtil.writeAllureEnvironmentXml;
+import static com.github.dwilliamsgs.utils.AllureEnvironmentUtil.*;
 import static org.testng.AssertJUnit.assertTrue;
 
 public class AllureEnvironmentUtilTest {
@@ -26,13 +26,13 @@ public class AllureEnvironmentUtilTest {
   @Test
   public void hasAllureEnvironmentXmlBeenCreated() {
     writeAllureEnvironmentXml(parameters);
-    assertTrue(new File(AllureEnvironmentUtil.DEFAULT_ALLURE_DIRECTORY + AllureEnvironmentUtil.DEFAULT_ALLURE_FILENAME).isFile());
+    assertTrue(new File(DEFAULT_ALLURE_DIRECTORY + DEFAULT_ALLURE_FILENAME).isFile());
   }
 
   @Test
   public void hasCustomAllureEnvironmentXmlBeenCreated() {
     String customAllureDirectory = "target/allure-results-custom/";
     writeAllureEnvironmentXml(parameters, customAllureDirectory);
-    assertTrue(new File(customAllureDirectory + AllureEnvironmentUtil.DEFAULT_ALLURE_FILENAME).isFile());
+    assertTrue(new File(customAllureDirectory + DEFAULT_ALLURE_FILENAME).isFile());
   }
 }
